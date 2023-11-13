ad70b6b3ceaa3269941f8f18bc3b756531dad10a
Richard Andrew Long <idrew@Richards-Mac-mini.local>
2023-11-12 21:27:51 -0600

    lesson 1

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there!")
+}
+
+// Questions
+
+// 1). How do we run the code in the project?
+// In the terminal < go run main.go
+
+// 2). What does 'import "fmt" mean?
+// used to give our package access to some code written in another package
+
+// 3). What does 'package main' mean?
+// A collection of common source code files
+
+// 4). Whats the 'func' thing?
+// declares a function, sets the name of the function, list the arguments and call the functionn to run the code
+
+// 5). How is the main.go file organized?
+// package main - import "fmt" - func (etc.)
